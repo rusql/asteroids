03d5b2f05fccffb1bc12280b7fa48cc94730edac
rusql <russell.subscriptions@gmail.com>
2024-09-15 15:09:46 +0200

    add shoot limiting refactoring

--- player.py
+++ player.py
@@ -7,7 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self.timer = 0
+        self.shoot_timer = 0
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -24,7 +24,7 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
-        self.timer -= dt
+        self.shoot_timer -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
@@ -39,9 +39,9 @@ class Player(CircleShape):
             self.shoot()
 
     def shoot(self):
-        if self.timer > 0:
+        if self.shoot_timer > 0:
             return
-        self.timer = PLAYER_SHOOT_COOLDOWN
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
