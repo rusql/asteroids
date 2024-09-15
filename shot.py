25db18d044de0fa0f6d7341375824f8526b452be
rusql <russell.subscriptions@gmail.com>
2024-09-15 14:57:59 +0200

    add shooting

--- shot.py
+++ shot.py
@@ -6,8 +6,9 @@ class Shot(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, SHOT_RADIUS)
 
-        def draw(self, screen):
-            pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
-        def update(self, dt):
-            self.position += (self.velocity * dt)
+    def update(self, dt):
+        self.position += (self.velocity * dt)
+
